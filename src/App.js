22d08cf81c92118784e9f68d5cb8b255d6b290c3
aniiaham <aniya_axm@icloud.com>
2022-11-10 18:46:38 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+const REACT_APP_API_KEY = "DEMO_KEY"
 
 function App() {
+  const [nasaData, setNasaData] = useState("");
+
+  useEffect(() => {
+    const fetchNasa = () => {
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=${REACT_APP_API_KEY}`)
+        .then((res) => {
+          // console.log(res)
+          setNasaData(res.data);
+          console.log(res);
+        })
+        .catch((err) => {
+          // console.log(err);
+        });
+    };
+    fetchNasa();
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Nasa title={nasaData.title} url={nasaData.url} explanation={nasaData.explanation} date={nasaData.date}/>
     </div>
   );
 }
 
 export default App;
+
+const Nasa = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <img src={props.url} alt="Nasa Image"/>
+      <p>{props.explanation}</p>
+      <p>{props.date}</p>
+    </div>
+  );
+};
